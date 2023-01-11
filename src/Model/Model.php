b4c46412611b47fcd8fdbe3595bf1936ad832a64
ratana <hengratana07@gmail.com>
2023-01-11 14:09:16 +0700

    Added: create function

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -172,6 +172,20 @@ class Model extends BaseModel
     }
 
     /**
+     * Save a new model and return the instance.
+     *
+     * @param  array  $fillables
+     * @param  array  $options
+     * @return \Kitar\Dynamodb\Model\Model|$this
+     */
+    public static function create(array $fillables = [], array $options = [])
+    {
+        $instance = new static($fillables);
+        $instance->save($options);
+        return $instance;
+    }
+
+    /**
      * @inheritdoc
      */
     protected function incrementOrDecrement($column, $amount, $extra, $method)
